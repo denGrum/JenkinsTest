359f387936c3c7537844f683fd6010f236436251
denGrum <den_grum@mail.ru>
2022-01-15 00:30:11 +0300

    *

--- src/test/java/end2end/HoverTest.java
+++ src/test/java/end2end/HoverTest.java
@@ -4,7 +4,6 @@ import base.Start;
 import flow.HoverFlow;
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
-import lombok.SneakyThrows;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
